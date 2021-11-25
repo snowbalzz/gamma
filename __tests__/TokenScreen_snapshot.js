0c72384ab735877e9ca447baa5c868a705793bd4
snowbalzz <alek0007@hz.nl>
2021-11-25 11:32:06 +0100

    Deletion of files

--- __tests__/TokenScreen_snapshot.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { create } from "react-test-renderer";
-import TokenScreen from "../app/screens/TokenScreen"
-
-const tree = create(<TokenScreen/>);
-
-test('snapshot', ()=>{
-    expect(tree).toMatchSnapshot();
-});
